76d0029d87737431297399822143ccb2bcdfb72c
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2023-07-25 17:03:57 -0500

    Added to contract: roylaty extension, errors, token limit, mint price, uri mechanism, events for mint and payout, parameters for constructor, reentrancyguard, reverts, receive, fallback

--- contracts/NFTContract.sol
+++ contracts/NFTContract.sol
@@ -3,39 +3,97 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+error NFTContract__MaxSupplyReached();
+error NFTContract__ValueNotEqualPrice();
+error NFTContract__WrongAvenueForThisTransaction();
 
 contract NFTContract is
     ERC721,
     ERC721Enumerable,
+    ERC721Burnable,
     ERC721URIStorage,
-    Pausable,
+    ERC721Royalty,
     Ownable,
-    ERC721Burnable
+    ReentrancyGuard
 {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    uint256 private immutable i_mint_price;
+    uint256 private immutable i_max_tokens;
+    string private s_base_uri;
+    address private immutable i_owner;
 
-    constructor() ERC721("NFT-Contract", "DGS") {}
+    event MintingCompleted(uint tokenId, address owner);
+    event FundsDistributed(address owner, uint amount);
 
-    function pause() public onlyOwner {
-        _pause();
+    constructor(
+        uint256 _mint_price,
+        uint256 _max_tokens,
+        string memory _base_uri,
+        address _royaltyArtist,
+        uint96 _royaltyBasis
+    ) ERC721("NFT-Contract", "DGS") {
+        i_mint_price = _mint_price;
+        i_max_tokens = _max_tokens;
+        s_base_uri = _base_uri;
+        _setDefaultRoyalty(_royaltyArtist, _royaltyBasis);
+        i_owner = msg.sender;
     }
 
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function mintTo(
+        string calldata uri // ipfs url string
+    ) public payable nonReentrant returns (uint256) {
         uint256 tokenId = _tokenIdCounter.current();
+        // check for supply limits
+        if (tokenId >= i_max_tokens) {
+            revert NFTContract__MaxSupplyReached();
+        }
+        // make sure there is money
+        if (msg.value != i_mint_price) {
+            revert NFTContract__ValueNotEqualPrice();
+        }
         _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        uint256 newItemId = _tokenIdCounter.current();
+        _safeMint(msg.sender, newItemId);
+        emit MintingCompleted(newItemId, msg.sender);
+        super._setTokenURI(tokenId, uri);
+        payable(i_owner).transfer(address(this).balance);
+        // let platform know everyone was paid
+        emit FundsDistributed(i_owner, msg.value);
+        // let us know new token id
+        return newItemId;
+    }
+
+    function getMaxSupply() public view returns (uint256) {
+        return i_max_tokens;
+    }
+
+    function getMintPrice() public view returns (uint256) {
+        return i_mint_price;
+    }
+
+    function getBaseURI() public view returns (string memory) {
+        return s_base_uri;
+    }
+
+    function setRoyalty(
+        // called by platform to set roylaty rates and artist payout address
+        address _receiver,
+        uint96 feeNumerator
+    ) public onlyOwner {
+        _setDefaultRoyalty(_receiver, feeNumerator);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return s_base_uri;
     }
 
     function _beforeTokenTransfer(
@@ -43,7 +101,7 @@ contract NFTContract is
         address to,
         uint256 tokenId,
         uint256 batchSize
-    ) internal override(ERC721, ERC721Enumerable) whenNotPaused {
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
@@ -51,7 +109,7 @@ contract NFTContract is
 
     function _burn(
         uint256 tokenId
-    ) internal override(ERC721, ERC721URIStorage) {
+    ) internal override(ERC721, ERC721URIStorage, ERC721Royalty) {
         super._burn(tokenId);
     }
 
@@ -66,7 +124,7 @@ contract NFTContract is
     )
         public
         view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        override(ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Royalty)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
