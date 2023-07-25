4b5dc767b6c18bf99aa2b6a3af832d5bff280192
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2023-07-25 16:35:15 -0500

    Added Mumbai Polygon to hardhat.config file and set MATIC / USD for gas-reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,20 +3,33 @@ require("dotenv").config();
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+const STUNT_WALLET_PRIVATE_KEY = process.env.STUNT_WALLET_PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 module.exports = {
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
+    outputFile: "gas-report.txt",
+    noColors: true,
   },
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 31337,
     },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [STUNT_WALLET_PRIVATE_KEY],
+    },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
+      accounts: [STUNT_WALLET_PRIVATE_KEY],
     },
   },
   solidity: {
